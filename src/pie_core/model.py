94a9227cd14cf4deb3f063d35029bf44a582db7a
Arne Binder <arne.binder@dfki.de>
2025-04-01 16:44:58 +0200

    make PieModelHFHubMixin abstract and implement save_model_file and load_model_file in PyTorchIEModel

--- src/pie_core/model.py
+++ src/pie_core/model.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 
+import torch
 from pytorch_lightning import LightningModule
 
 from pie_core.hf_hub_mixin import PieModelHFHubMixin
@@ -7,6 +8,19 @@ from pie_core.registrable import Registrable
 
 
 class PyTorchIEModel(PieModelHFHubMixin, LightningModule, Registrable):
+
+    def save_model_file(self, model_file: str) -> None:
+        """Save weights from a Pytorch model to a local directory."""
+        model_to_save: torch.nn.Module = self.module if hasattr(self, "module") else self
+        torch.save(model_to_save.state_dict(), model_file)
+
+    def load_model_file(
+        self, model_file: str, map_location: str = "cpu", strict: bool = False
+    ) -> None:
+        state_dict = torch.load(model_file, map_location=torch.device(map_location))
+        self.load_state_dict(state_dict, strict=strict)
+        self.eval()
+
     def _config(self) -> Dict[str, Any]:
         config = super()._config() or {}
         config[self.config_type_key] = PyTorchIEModel.name_for_object_class(self)
