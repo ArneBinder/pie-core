49ea4406b0661c27b13ffa605052489e3e94912f
Arne Binder <arne.binder@dfki.de>
2025-04-04 15:42:09 +0200

    add document tests

--- tests/common.py
+++ tests/common.py
@@ -41,8 +41,36 @@ class LabeledSpan(Span):
         return self.label, super().resolve()
 
 
+@dataclasses.dataclass(eq=True, frozen=True)
+class Label(Annotation):
+    label: str
+    score: float = dataclasses.field(default=1.0, compare=False)
+
+    def resolve(self) -> Any:
+        return self.label
+
+
 @dataclasses.dataclass
 class TextBasedDocument(Document):
     text: str
     id: Optional[str] = None
     metadata: Dict[str, Any] = dataclasses.field(default_factory=dict)
+
+
+@dataclasses.dataclass
+class TokenBasedDocument(Document):
+    tokens: tuple[str, ...]
+    id: Optional[str] = None
+    metadata: Dict[str, Any] = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self) -> None:
+        # When used in a dataset, the document gets serialized to json like structure which does not know tuples,
+        # so they get converted to lists. This is a workaround to automatically convert the "tokens" back to tuples
+        # when the document is created from a dataset.
+        if isinstance(self.tokens, list):
+            object.__setattr__(self, "tokens", tuple(self.tokens))
+        elif not isinstance(self.tokens, tuple):
+            raise ValueError("tokens must be a tuple.")
+
+        # Call the default document construction code
+        super().__post_init__()
