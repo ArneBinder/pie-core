e325695ec473488402920e2376b249028561a29c
r33v4 <rainbowrivey@hotmail.com>
2025-05-02 09:37:08 +0200

    test on simple class
    fix exception
    new tests

--- tests/test_preparable.py
+++ tests/test_preparable.py
@@ -2,68 +2,78 @@ import logging
 
 import pytest
 
-from tests.common.taskmodules import TestDocumentWithLabel, TestTaskModule
-from tests.common.types import Label
+from pie_core import PreparableMixin
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="module")
-def unprepared_taskmodule() -> TestTaskModule:
-    return TestTaskModule()
+class PreparableObject(PreparableMixin):
+    PREPARED_ATTRIBUTES = ["attr"]
+
+    def _prepare(self):
+        self.attr = True
 
 
 @pytest.fixture(scope="module")
-def documents() -> list[TestDocumentWithLabel]:
-    """
-    - Creates example documents with predefined texts.
-    - Assigns labels to the documents for testing purposes.
-
-    """
-    doc1 = TestDocumentWithLabel(text="May your code be bug-free and your algorithms optimized!")
-    doc2 = TestDocumentWithLabel(
-        text="A cascading failure occurred, resulting in a complete system crash and irreversible data loss."
-    )
-    # add labels
-    doc1.label.append(Label(label="Positive"))
-    doc2.label.append(Label(label="Negative"))
-    return [doc1, doc2]
+def prepared_object():
+    obj = PreparableObject()
+    obj.prepare()
+    return obj
 
 
 @pytest.fixture(scope="module")
-def prepared_taskmodule(documents) -> TestTaskModule:
-    """
-    - Prepares the task module with the given documents, i.e. collect available label values.
-    - Calls the necessary methods to prepare the task module with the documents.
-    - Calls _prepare(documents) and then _post_prepare()
-
-    """
-    taskmodule = TestTaskModule()
-    taskmodule.prepare(documents)
-    return taskmodule
+def unprepared_object():
+    obj = PreparableObject()
+    return obj
 
 
-def test_prepared_attributes(unprepared_taskmodule, prepared_taskmodule, documents):
+def test_is_prepared(unprepared_object, prepared_object):
+    assert prepared_object.is_prepared
+    assert not unprepared_object.is_prepared
 
-    assert not unprepared_taskmodule.is_prepared
+
+def test_prepared_attributes(unprepared_object, prepared_object):
+    assert not unprepared_object.is_prepared
     with pytest.raises(Exception) as excinfo:
-        attrs = unprepared_taskmodule.prepared_attributes
-    assert str(excinfo.value) == "The module is not prepared."
+        attrs = unprepared_object.prepared_attributes
+    assert str(excinfo.value) == "The PreparableObject is not prepared."
 
-    assert prepared_taskmodule.is_prepared
-    assert prepared_taskmodule.prepared_attributes == {"labels": ["Negative", "Positive"]}
+    assert prepared_object.is_prepared
+    assert prepared_object.prepared_attributes == {"attr": True}
 
 
-def test_assert_is_prepared(unprepared_taskmodule):
+def test_assert_is_prepared(unprepared_object):
     with pytest.raises(Exception) as excinfo:
-        unprepared_taskmodule.assert_is_prepared()
-    assert str(excinfo.value) == " Required attributes that are not set: ['labels']"
+        unprepared_object.assert_is_prepared()
+    assert str(excinfo.value) == "Required attributes that are not set: ['attr']"
 
 
-def test_prepare_prepared_taskmodule(prepared_taskmodule, documents, caplog):
+def test_prepare():
+    obj = PreparableObject()
+    assert not obj.is_prepared
+    obj.prepare()
+    assert obj.is_prepared
+
+
+def test_prepare_prepared_object(prepared_object, caplog):
     with caplog.at_level(logging.WARNING):
-        prepared_taskmodule.prepare(documents)
+        prepared_object.prepare()
     assert (
-        "The TestTaskModule is already prepared, do not prepare again.\nlabels = ['Negative', 'Positive']"
+        "The PreparableObject is already prepared, do not prepare again.\nattr = True"
         in caplog.text
+    )
+
+
+def test_prepare_with_bad_prepare_impl():
+    obj = PreparableObject()
+
+    def bad_prepare():
+        pass
+
+    obj._prepare = bad_prepare
+    with pytest.raises(Exception) as excinfo:
+        obj.prepare()
+    assert str(excinfo.value) == (
+        "_prepare() was called, but the PreparableObject is not prepared. "
+        "Required attributes that are not set: ['attr']"
     )
