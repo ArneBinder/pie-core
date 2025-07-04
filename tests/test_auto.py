567a89f64cc18094d549a612209551bddbdf4535
Danylo Mysak <RainbowRivey@hotmail.com>
2025-07-04 19:12:40 +0200

    Add from_pretrained() tests
    Do not create AutoTest class instance

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -1,12 +1,35 @@
-from typing import Optional
+from pathlib import Path
+from typing import Any, Dict, Optional, Type, Union
+
+import pytest
 
 from pie_core import Auto, Registrable
-from pie_core.hf_hub_mixin import HFHubMixin
+from pie_core.hf_hub_mixin import HFHubMixin, T
+from tests import FIXTURES_ROOT
+
+CONFIG_PATH = FIXTURES_ROOT / "configs"
 
 
 class TestHFHubMixin(HFHubMixin):
-    config_name: str = "auto_config.json"
-    config_type_key: str = "auto_config_type"
+    config_name: str = "test_config.json"
+    config_type_key: str = "test_config_type"
+
+    @classmethod
+    def _from_pretrained(
+        cls: Type[HFHubMixin],
+        *,
+        model_id: str,
+        revision: Optional[str],
+        cache_dir: Optional[Union[str, Path]],
+        force_download: bool,
+        proxies: Optional[Dict],
+        resume_download: bool,
+        local_files_only: bool,
+        token: Optional[Union[str, bool]],
+        config: Optional[dict] = None,
+        **kwargs,
+    ) -> HFHubMixin:
+        return cls.from_config(config=config or {}, **kwargs)
 
 
 class Test(Registrable, TestHFHubMixin):
@@ -33,17 +56,32 @@ class AutoTest(TestHFHubMixin, Auto[Test]):
     BASE_CLASS = Test
 
 
-def test_from_config():
-    auto_test = AutoTest()
-    sub = auto_test.from_config(config={"auto_config_type": "Sub", "foo": "Test"})
+@pytest.fixture(scope="module")
+def config_as_dict() -> Dict[str, Any]:
+    return {"test_config_type": "Sub", "foo": "Test"}
+
+
+def test_from_config(config_as_dict):
+    sub = AutoTest.from_config(config=config_as_dict)
     assert isinstance(sub, Sub)
     assert sub.foo == "Test"
 
 
-def test_from_config_with_kwargs_override():
-    auto_test = AutoTest()
-    sub = auto_test.from_config(
-        config={"auto_config_type": "Sub", "foo": "Test"}, auto_config_type="Sub2", foo="Test2"
-    )
+def test_from_config_with_kwargs_override(config_as_dict):
+    sub = AutoTest.from_config(config=config_as_dict, test_config_type="Sub2", foo="Test2")
     assert isinstance(sub, Sub2)
+    assert sub.foo == "Test2"
+
+
+def test_from_pretrained():
+    sub = AutoTest.from_pretrained(CONFIG_PATH)
+    assert isinstance(sub, Sub)
+    assert sub.is_from_pretrained
+    assert sub.foo == "Test"
+
+
+def test_from_pretrained_with_kwargs_override():
+    sub = AutoTest.from_pretrained(CONFIG_PATH, test_config_type="Sub2", foo="Test2")
+    assert isinstance(sub, Sub2)
+    assert sub.is_from_pretrained
     assert sub.foo == "Test2"
